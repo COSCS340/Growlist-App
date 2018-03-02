92d58badf6ab6fc418c0393c704534da77442b7c
Brandon Yen <byen@vols.utk.edu>
2018-03-02 10:33:30 -0500

    Modified to support returning to the main activity along with user input.

--- src/ListView_src/src/main/java/comcoscs340growlist_app/httpsgithub/growlist/addItemActivity.java
+++ src/ListView_src/src/main/java/comcoscs340growlist_app/httpsgithub/growlist/addItemActivity.java
@@ -1,7 +1,11 @@
-package comcoscs340growlist_app.httpsgithub.growlist;
+package com.example.byen.localgrowlistproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
 //This is blank for now
 public class addItemActivity extends AppCompatActivity {
 
@@ -11,5 +15,26 @@ public class addItemActivity extends AppCompatActivity {
         //for now, the activity will be static. If longer descriptions are needed,
         // this can be changed
         setContentView(R.layout.activity_add_item);
+
+
+    }
+    //perform some checking on the input
+    //add to database
+    //return to main activity
+    //Main activity still needs to be modified to receive the input
+    public void onClick(View view) {
+        //may want to do some checks/verification on the input before returning
+        Intent toMain = new Intent(this, MainActivity.class);
+
+        final EditText speciesName = (EditText) findViewById(R.id.speciesNameInput);
+        final EditText speciesDescription = (EditText) findViewById(R.id.speciesDescriptionInput);
+
+        String sendName = speciesName.getText().toString();
+        String sendDescription = speciesDescription.getText().toString();
+
+        toMain.putExtra("addItemName", sendName);
+        toMain.putExtra("addItemDescription", sendDescription);
+
+        startActivity(toMain);
     }
 }
